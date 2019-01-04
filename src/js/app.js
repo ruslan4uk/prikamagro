a80964c4a070a03620393205ebb3dbe025c613e2
Ruslan Safin <rusel91@idz.ru>
2019-01-04 18:32:34 +0400

    main page

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,117 @@
-import 'jquery';
+import $ from 'jquery';
 import 'popper.js';
 import 'bootstrap';
+import '@fortawesome/fontawesome-free';
+
+var app = {
+  'config': {
+    'search': {
+      'open': '.js-search-panel',
+      'group': '.search__group',
+      'btn': ''
+    },
+    'mainSlide': {
+      'btn': '.main-slide__link',
+      'event': 'mouseover click',
+      'activeClass': 'active',
+      'bgContainer': '.main-slide',
+      'img': '',
+    },
+  },
+};
+
+((window, document, $, app) => {
+
+  var $doc = $(document);
+
+  /**
+   * Search panel open and close
+   */
+  app.search = {
+    init: () => {
+      if ($(app.config.search.open).length) {
+        $doc.on('click touchend', app.config.search.open, () => {
+          if ($(app.config.search.group).is(':visible')) {
+            app.search.close();
+          } else {
+            app.search.open();
+          }
+        });
+      }
+    },
+    // function open search panel and switch button
+    open: () => {
+      $(app.config.search.group).show().animate({
+        opacity: '1'
+      }, 150);
+      $(app.config.search.open).addClass('active');
+    },
+    // function close search panel and switch button
+    close: () => {
+      $(app.config.search.group).animate({
+        opacity: '0'
+      }, 150, () => {
+        $(app.config.search.group).hide();
+      });
+      $(app.config.search.open).removeClass('active');
+    }
+  };
+
+  /**
+   * Main slide background swich
+   * app.config.mainSlide.btn
+   * app.config.mainSlide.event
+   * app.config.mainSlide.activeClass
+   */
+  app.mainSlide = {
+    init: () => {
+      $doc.on(app.config.mainSlide.event, app.config.mainSlide.btn, function (e) {
+        e.stopPropagation();
+        e.preventDefault();
+        $(app.config.mainSlide.btn).removeClass(app.config.mainSlide.activeClass);
+        $(this).addClass(app.config.mainSlide.activeClass);
+        app.mainSlide.imgSwich($(this).data('slide'));
+      });
+    },
+    imgSwich: (slide) => {
+      if (slide) {
+        $('[data-img]').removeClass(app.config.mainSlide.activeClass);
+        $('[data-img="' + slide + '"]').addClass(app.config.mainSlide.activeClass);
+      }
+    }
+  };
+
+
+  /**
+   * Location href to data attribute
+   * data-fakehref
+   */
+  app.fakeHref = {
+    init: () => {
+      $doc.on('click touchend', '[data-fakehref]', function (e) {
+        e.preventDefault();
+        window.location.pathname = $(this).data('fakehref');
+      });
+    }
+  };
+
+
+
+
+  $doc.ready(() => {
+    /**
+     * Init top search panel
+     */
+    app.search.init();
+    /**
+     * Init slide to main page
+     */
+    app.mainSlide.init();
+    /**
+     * Init 
+     */
+    app.fakeHref.init();
+  });
+
+
+})(window, document, $, app);
