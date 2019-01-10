de2b40e3931c9a79b088b3cd330d6a542f6e2d37
Ruslan Safin <rusel91@idz.ru>
2019-01-10 16:41:49 +0400

    add page

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ import 'popper.js';
 import 'bootstrap';
 import '@fortawesome/fontawesome-free';
 import 'owl.carousel2';
+import '@fancyapps/fancybox';
 
 var app = {
   'config': {
@@ -18,12 +19,33 @@ var app = {
       'bgContainer': '.main-slide',
       'img': '',
     },
+    'burger': {
+      'btn': '.burger',
+      'event': 'click touchend',
+      'nav': '.mobile',
+      'openClass': 'mobile--is-open',
+      'close': '.mobile__close',
+    },
   },
 };
 
 ((window, document, $, app) => {
 
   var $doc = $(document);
+
+  /**
+   * Parallax
+   */
+  app.parallax = {
+    init: (el, speed) => {
+      $(window).on('scroll', function() {
+        var scroll = $(document).scrollTop();
+        $(el).css({
+          'background-position': '50% ' + (-speed * scroll) + 'px'
+        });
+      });
+    },
+  };
 
   /**
    * Start carousel
@@ -75,7 +97,7 @@ var app = {
    */
   app.mainSlide = {
     init: () => {
-      $doc.on(app.config.mainSlide.event, app.config.mainSlide.btn, function (e) {
+      $doc.on(app.config.mainSlide.event, app.config.mainSlide.btn, function(e) {
         e.stopPropagation();
         e.preventDefault();
         $(app.config.mainSlide.btn).removeClass(app.config.mainSlide.activeClass);
@@ -98,9 +120,50 @@ var app = {
    */
   app.fakeHref = {
     init: () => {
-      $doc.on('click touchend', '[data-fakehref]', function (e) {
+      $doc.on('click touchend', '[data-fakehref]', function(e) {
         e.preventDefault();
         window.location.pathname = $(this).data('fakehref');
+      });
+    }
+  };
+
+  /**
+   * burger menu
+   */
+  app.burger = {
+    init: () => {
+      $doc.on(app.config.burger.event, app.config.burger.btn, function(e) {
+        e.preventDefault();
+        $(app.config.burger.nav).addClass(app.config.burger.openClass);
+        app.burger.addOverlay();
+      }).on(app.config.burger.event, app.config.burger.close, function(e) {
+        e.preventDefault();
+        $(app.config.burger.nav).removeClass(app.config.burger.openClass);
+        app.burger.removeOverlay();
+      });
+    },
+    addOverlay: () => {
+      //window.nav.height = $(app.config.burger.nav).height();
+      $('body').css({
+        'overflow': 'hidden',
+        'height': $(app.config.burger.nav).height()
+      });
+    },
+    removeOverlay: () => {
+      $('body').css({
+        'overflow': '',
+        'height': '',
+      });
+    },
+  };
+
+  app.fancy = {
+    init: () => {
+      $('[data-fancybox]').fancybox({
+        slideShow  : true,
+        fullScreen : true,
+        thumbs     : true,
+        closeBtn   : true,
       });
     }
   };
@@ -130,7 +193,34 @@ var app = {
       items: 1,
       navText: ['<svg><use xlink: href = "img/sprite.svg#icon-back"></use></svg>', '<svg><use xlink: href = "img/sprite.svg#icon-back"></use></svg>'],
     });
+    /**
+     * Parallax init
+     */
+    app.parallax.init('.jumbotron', '0.25');
+    /**
+     * Burger init
+     */
+    app.burger.init();
+    /**
+     * Fancybox me app init
+     */
+    //app.fancy.init();
   });
 
 
 })(window, document, $, app);
+
+
+/**
+ * Scroll to anchor
+ */
+$(document).ready(function() {
+  $('a[href*=#]').on('click', function(e) {
+    var anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $(anchor.attr('href')).offset().top
+    }, 777);
+    e.preventDefault();
+    return false;
+  });
+});
